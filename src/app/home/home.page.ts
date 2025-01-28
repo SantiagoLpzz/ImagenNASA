b9cdcf9b211f8d8b72d5055c72f9802896e5b6bc
SantiagoLpzz <22014@portalcedi.edu.mx>
2025-01-28 10:35:20 -0600

    FinalAct1

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NasaService } from '../nasa.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage {
+
+  imageData: any;
+
+  constructor(private nasaService: NasaService) {}
+
+  ngOnInit(){
+    this.nasaService.getImageOfTheDay().subscribe((data) => {
+      this.imageData = data;
+    });
+  }
+
+}
